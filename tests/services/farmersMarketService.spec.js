8a6dcee8bcd0deaf87954955a4f167d574601155
Michael Cooper <macoop2363@gmail.com>
2020-02-19 14:06:31 -0700

    Add test for getting farmers markets by zip code

---
+++ tests/services/farmersMarketService.spec.js
@@ -0,0 +1,16 @@
+var shell = require('shelljs');
+var request = require("supertest");
+var musixMatchService = require('../../lib/services/farmersMarketService');
+
+describe('Test ', () => {
+  test('It should get a json response with a list of markets from the Farmers Market API', async () => {
+    let response = await farmersMarketService.getMarketsByZip(80303)
+
+    expect(response).toHaveProperty('results')
+    expect(response.results).toHaveProperty('id')
+    expect(response.results).toHaveProperty('marketname')
+  });
+
+  test('It should get a json response with the details of a specific market', async () => {
+  });
+});
