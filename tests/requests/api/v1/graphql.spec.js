82d9f41e775887208b61e8e3a30ac626a57a0bb9
Michael Cooper <macoop2363@gmail.com>
2020-02-21 16:40:46 -0700

    Add additional resources into the database for testing GraphQL queries

--- tests/requests/api/v1/graphql.spec.js
+++ tests/requests/api/v1/graphql.spec.js
@@ -8,8 +8,9 @@ const database = require('knex')(configuration);
 describe('Test The Market\'s Path', () => {
   beforeEach(async () => {
     await database.raw('TRUNCATE TABLE markets CASCADE')
+    await database.raw('TRUNCATE TABLE vendors CASCADE')
     
-    await database('markets').insert({
+    let market_1 = await database('markets').insert({
       'id': 123345,
       'name': "market_1",
       'address': "market_1_address",
@@ -17,8 +18,9 @@ describe('Test The Market\'s Path', () => {
       'schedule': "market_1_schedule",
       'latitude': 0.99,
       'longitude': 0.99
-    })
-    await database('markets').insert({
+    }, 'id')
+    
+    let market_2 = await database('markets').insert({
       'id': 67890,
       'name': "market_2",
       'address': "market_2_address",
@@ -26,17 +28,74 @@ describe('Test The Market\'s Path', () => {
       'schedule': "market_2_schedule",
       'latitude': 0.99,
       'longitude': 0.99
+    }, 'id')
+
+    let vendor_1 = await database('vendors').insert({
+      name: "vendor_1",
+      description: "vendor_1_description_1",
+      image_link: "vendor_1_image_link_1",
+    }, 'id')
+
+    let vendor_2 = await database('vendors').insert({
+      name: "vendor_2",
+      description: "vendor_2_description_2",
+      image_link: "vendor_2_image_link_2",
+    }, 'id')
+
+    let market_vendor_1 = await database('market_vendors').insert({
+      market_id: market_1[0],
+      vendor_id: vendor_1[0]
     })
+
+    let market_vendor_2 = await database('market_vendors').insert({
+      market_id: market_1[0],
+      vendor_id: vendor_2[0]
+    })
+
+    let market_vendor_3 = await database('market_vendors').insert({
+      market_id: market_2[0],
+      vendor_id: vendor_1[0]
+    })
+
+    let vendor_1_products = await database('products').insert([
+      {
+        name: "vendor_1_product_1",
+        description: "vendor_1_product_1_description_1",
+        price: 0.99,
+        vendor_id: vendor_1[0]
+      },
+      {
+        name: "vendor_1_product_2",
+        description: "vendor_1_product_2_description_2",
+        price: 0.99,
+        vendor_id: vendor_1[0]
+      }
+    ])
+
+    let vendor_2_products = await database('products').insert([
+      {
+        name: "vendor_2_product_1",
+        description: "vendor_2_product_1_description_1",
+        price: 0.99,
+        vendor_id: vendor_2[0]
+      },
+      {
+        name: "vendor_2_product_2",
+        description: "vendor_2_product_2_description_2",
+        price: 0.99,
+        vendor_id: vendor_2[0]
+      }
+    ])
   })
   afterEach(() => {
     database.raw('TRUNCATE TABLE markets CASCADE')
+    database.raw('TRUNCATE TABLE vendors CASCADE')
   })
 
   it('should test happy path', async () => {
     let res = await request(app)
       .get('/api/v1/graphql?query={markets{id name address google_link schedule latitude longitude}}')
       
-      console.log(res.body)
     expect(res.statusCode).toBe(200)
     expect(res.body).toHaveProperty('data')
     expect(res.body.data.markets.length).toBe(2)
