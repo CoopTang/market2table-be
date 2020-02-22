531e322a9e689fa91a4fd212ef32ea73b382bba3
Michael Cooper <macoop2363@gmail.com>
2020-02-22 13:13:14 -0700

    Add test to make sure markets are seeded when the markets endpoint is queried

--- tests/requests/api/v1/markets.spec.js
+++ tests/requests/api/v1/markets.spec.js
@@ -6,25 +6,29 @@ const configuration = require('../../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
 describe('Test The Market\'s Path', () => {
-    beforeEach(async () => {
-        await database.raw('TRUNCATE TABLE markets CASCADE')
-    })
-    afterEach(() => {
-        database.raw('TRUNCATE TABLE markets CASCADE')
-    })
+  beforeEach(async () => {
+    await database.raw('TRUNCATE TABLE markets CASCADE')
+  })
+  afterEach(() => {
+    database.raw('TRUNCATE TABLE markets CASCADE')
+  })
 
-    it('should test happy path', async () => {
-        let res = await request(app)
-        .get('/api/v1/markets?zip=80203')
+  it('should test happy path', async () => {
+    let res = await request(app)
+      .get('/api/v1/markets?zip=80203')
 
-        expect(res.statusCode).toBe(200)
-        expect(res.body.length).toBe(19)
-        expect(res.body[0]).toHaveProperty('id')
-        expect(res.body[0]).toHaveProperty('name')
-        expect(res.body[0]).toHaveProperty('address')
-        expect(res.body[0]).toHaveProperty('google_link')
-        expect(res.body[0]).toHaveProperty('schedule')
-        expect(res.body[0]).toHaveProperty('latitude')
-        expect(res.body[0]).toHaveProperty('longitude')
-    })
+    let markets = database('markets').select()
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body.length).toBe(19)
+    expect(res.body[0]).toHaveProperty('id')
+    expect(res.body[0]).toHaveProperty('name')
+    expect(res.body[0]).toHaveProperty('address')
+    expect(res.body[0]).toHaveProperty('google_link')
+    expect(res.body[0]).toHaveProperty('schedule')
+    expect(res.body[0]).toHaveProperty('latitude')
+    expect(res.body[0]).toHaveProperty('longitude')
+    expect(markets.length).toBe(19)
+    
+  })
 })
